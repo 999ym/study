a58453ef4a5d0471a090a8f8268b1cf187185868
9yeonmi <yeonmi47@gmail.com>
2022-07-17 21:58:17 +0900

    220717 C언어

---
+++ c/1강/1강/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(void) {
+	/*
+	//챕터1
+	//기본 출력
+
+	printf("hello world");
+	system("pause");
+	return 0;
+	
+	*/
+
+
+	/*
+	//챕터2
+	//변수와 상수
+	//main 함수밖의 변수는 정적변수라 부르며 초기화값은 0이 들어감
+
+	int a = 7; //초기화 되지않은 변수는 쓰레기값이 들어감
+	printf("The number is %d.\n", a);
+	system("pause");
+	return 0;
+	
+	
+	*/
+
+}
